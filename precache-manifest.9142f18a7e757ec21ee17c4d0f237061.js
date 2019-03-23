e118138a12816b3cce83e8f5e349938ba692c1f0
John Yu <john.yu@reisystems.com>
2019-03-23 08:31:23 -0400

    Updates

---
+++ precache-manifest.9142f18a7e757ec21ee17c4d0f237061.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d715a52fe0d178ea9ddd",
+    "url": "/MineSweeper3D/static/css/main.148b210f.chunk.css"
+  },
+  {
+    "revision": "d715a52fe0d178ea9ddd",
+    "url": "/MineSweeper3D/static/js/main.2b0c19f0.chunk.js"
+  },
+  {
+    "revision": "1c0a69c25e9c70e975a4",
+    "url": "/MineSweeper3D/static/js/runtime~main.3f345a18.js"
+  },
+  {
+    "revision": "d4a11412c8932457d724",
+    "url": "/MineSweeper3D/static/js/2.316988c9.chunk.js"
+  },
+  {
+    "revision": "6ae98a6bc4caedaadf1396279266d6e0",
+    "url": "/MineSweeper3D/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MineSweeper3D/precache-manifest.9142f18a7e757ec21ee17c4d0f237061.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MineSweeper3D/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
